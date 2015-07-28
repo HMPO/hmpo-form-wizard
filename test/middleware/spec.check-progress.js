c28027302a3a118e4ce9da9f76fb5eb477a13a61
Leonard Martin <leonard.martin@gmail.com>
2015-07-28 16:00:46 +0100

    Shift responsibility for handling missing pre-requisites from middleware to controller
    Handling missing pre-requisites at the middleware level prevents custom behaviour from being bound. Instead shift the handling for missing pre-requisites into the controller so that implementations can extend the methods to create their own custom handling.

--- test/middleware/spec.check-progress.js
+++ test/middleware/spec.check-progress.js
@@ -1,30 +1,41 @@
 var checkSession = require('../../lib/middleware/check-progress'),
-    Model = require('../../lib/model');
+    Model = require('../../lib/model'),
+    Controller = require('../../lib/controller');
 
 describe('middleware/check-session', function () {
 
-    var req, res, next, controller;
+    var req, res, next, controller, steps;
 
     beforeEach(function () {
         req = request();
         req.sessionModel = new Model({}, { session: req.session, key: 'test' });
         res = response();
         next = sinon.stub();
-        controller = {
-            on: function () {},
-            options: {
-                fields: {
-                    field1: {},
-                    field2: undefined
-                }
-            }
-        };
+        controller = new Controller({ template: 'index' });
+        steps = {
+            '/one': { next: '/two' },
+            '/two': { next: '/three' },
+            '/three': { next: '/four' },
+            '/four': {}
+        }
     });
 
-    it('does not throw if some fields are undefined', function () {
-        var middleware = checkSession('/route', controller, {}, '/first');
-        middleware(req, res, next);
-        next.should.have.been.calledWithExactly();
+    it('calls callback with no arguments if prerequisite steps are complete', function (done) {
+        req.sessionModel.set('steps', [ '/one', '/two' ]);
+        var middleware = checkSession('/three', controller, steps, '/one');
+        middleware(req, res, function (err) {
+            expect(err).to.be.undefined;
+            done();
+        });
+    });
+
+    it('calls callback with MISSING_PREREQ error code if accessing step that has not had prerequisite steps complete', function (done) {
+        req.sessionModel.set('steps', []);
+        var middleware = checkSession('/three', controller, steps, '/one');
+        middleware(req, res, function (err) {
+            err.code.should.equal('MISSING_PREREQ');
+            done();
+        });
     });
 
 });
