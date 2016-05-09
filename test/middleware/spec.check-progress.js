dd7368adbadd73508de5fd30e5a46e2ac1af1ca7
joefitter <hello@joefitter.com>
2016-05-09 11:52:15 +0100

    Closes #53 - invalidate all unreachable steps on forking
    * Check for forks on controller complete, if present then exec invalidation logic
    * Get all reachable steps from paths not taken, and path taken, invalidate all in blacklist that aren't present in whitelist
    * invalidation removes step and all step fields from sessionModel

--- test/middleware/spec.check-progress.js
+++ test/middleware/spec.check-progress.js
@@ -49,4 +49,164 @@ describe('middleware/check-session', function () {
         });
     });
 
+    describe('step/field invalidating logic', function () {
+        var sessionFields, sessionSteps;
+
+        beforeEach(function () {
+            steps = {
+                '/': {
+                    next: '/step1'
+                },
+                '/step1': {
+                    next: '/step2',
+                    fields: [
+                        'step-1-field-1',
+                        'step-1-field-2'
+                    ],
+                    forks: [{
+                        target: '/fork1'
+                    }, {
+                        target: '/step4'
+                    }]
+                },
+                '/step2': {
+                    next: '/step3',
+                    fields: [
+                        'step-2-field-1',
+                        'step-2-field-2'
+                    ],
+                    forks: [{
+                        target: '/step4'
+                    }]
+                },
+                '/step3': {
+                    next: '/step4',
+                    fields: [
+                        'step-3-field-1',
+                        'step-3-field-2'
+                    ]
+                },
+                '/fork1': {
+                    next: '/step2',
+                    fields: [
+                        'fork-field-1',
+                        'fork-field-2'
+                    ]
+                },
+                '/step4': {
+                    fields: [
+                        'step-4-field-1',
+                        'step-4-field-2'
+                    ]
+                }
+            };
+
+            sessionFields = {
+                'step-1-field-1': true,
+                'step-1-field-2': true,
+                'step-2-field-1': true,
+                'step-2-field-2': true,
+                'step-3-field-1': true,
+                'step-3-field-2': true,
+                'step-4-field-1': true,
+                'step-4-field-2': true,
+                'fork-field-1': true,
+                'fork-field-2': true
+            };
+
+            sessionSteps = ['/step1', '/step2', '/step3', '/fork1', '/step4'];
+
+            req.sessionModel.set(sessionFields);
+            req.sessionModel.set({
+                steps: sessionSteps
+            });
+
+            controller.options.next = steps['/step1'].next;
+            controller.options.forks = steps['/step1'].forks;
+
+            checkProgress('/step1', controller, steps, '/');
+        });
+
+        describe('when all steps are still reachable after forking', function () {
+            beforeEach(function () {
+                controller.getNextStep = sinon.stub().returns('/fork1');
+                controller.emit('complete', req, res);
+            });
+
+            it('no steps are removed', function () {
+                req.sessionModel.get('steps').should.be.eql(sessionSteps);
+            });
+
+            it('doesn\'t invalidate any fields', function () {
+                req.sessionModel.toJSON().should.contain.all.keys(
+                    'step-1-field-1',
+                    'step-1-field-2',
+                    'step-2-field-1',
+                    'step-2-field-2',
+                    'step-3-field-1',
+                    'step-3-field-2',
+                    'step-4-field-1',
+                    'step-4-field-2',
+                    'fork-field-1',
+                    'fork-field-2'
+                );
+            });
+        });
+
+        describe('when a step is skipped', function () {
+
+            beforeEach(function () {
+                controller.getNextStep = sinon.stub().returns('/step2');
+                controller.emit('complete', req, res);
+            });
+
+            it('invalidates skipped step', function () {
+                req.sessionModel.get('steps').should.not.contain('/fork1');
+            });
+
+            it('invalidates fields associated with skipped step', function () {
+                expect(req.sessionModel.get('fork-field-1')).to.be.undefined;
+                expect(req.sessionModel.get('fork-field-2')).to.be.undefined;
+            });
+
+            it('doens\'t invalidate the \'next\' step in the current journey', function () {
+                req.sessionModel.get('steps').should.contain('/step2');
+            });
+
+            it('doesn\'t unset fields associated with the next step in the current journey', function () {
+                req.sessionModel.get('step-2-field-1').should.be.true;
+                req.sessionModel.get('step-2-field-2').should.be.true;
+            });
+
+        });
+
+        describe('when the result of forking skips multiple steps', function () {
+
+            beforeEach(function () {
+                controller.getNextStep = sinon.stub().returns('/step4');
+                controller.emit('complete', req, res);
+            });
+
+            it('invalidates skipped steps', function () {
+                req.sessionModel.get('steps').should.not.contain.any('/step2', '/step3', '/fork1');
+            });
+
+            it('doesn\'t invalidate steps in current journey', function () {
+                req.sessionModel.get('steps').should.contain.all('/step1', '/step4');
+            });
+
+            it('invalidates all fields associated with skipped steps', function () {
+                req.sessionModel.toJSON().should.not.contain.any.keys(
+                    'step-2-field-1',
+                    'step-2-field-2',
+                    'step-3-field-1',
+                    'step-3-field-2',
+                    'fork-field-1',
+                    'fork-field-2'
+                );
+            });
+        });
+
+    });
+
 });
