bee5f217a8dce329f09d6deb6910e3efa135dfd7
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2016-05-23 15:14:22 +0100

    Update example modules and linting

--- example/controllers/start.js
+++ example/controllers/start.js
@@ -4,13 +4,13 @@ var Controller = require('hmpo-form-wizard').Controller;
 
 var Start = function () {
     Controller.apply(this, arguments);
-}
+};
 
 util.inherits(Start, Controller);
 
 Start.prototype.saveValues = function (req, res, callback) {
     req.sessionModel.reset();
     callback();
-}
+};
 
 module.exports = Start;
--- example/routes/index.js
+++ example/routes/index.js
@@ -1,11 +1,10 @@
 var steps = require('./steps'),
-    fields = require('./fields'),
-    i18n = require('i18next');
+    fields = require('./fields');
 
 var app = require('express').Router();
 
-app.use(require('hmpo-template-mixins')(i18n.t, fields));
+app.use(require('hmpo-template-mixins')(fields));
 
-app.use(require('../../')(steps, fields, { translate: i18n.t }));
+app.use(require('../../')(steps, fields));
 
 module.exports = app;
