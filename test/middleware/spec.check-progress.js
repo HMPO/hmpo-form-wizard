johndallen/hof-form-wizard

Rename getPreviousSteps helper to getRouteSteps and simplify tests
getPreviousSteps seems a little ambiguous, rename to getRouteSteps to differentiate from the user's total steps. Simplify the tests to improve the readability of them for documentation purposes.

Test that back-links and check-progress middleware use the new helper method.

test/middleware/spec.check-progress.js
@@ -1,18 +1,11 @@
-var proxyquire = require('proxyquire'),
+var checkSession = require('../../lib/middleware/check-progress'),
     Model = require('../../lib/model'),
-    Controller = require('../../lib/controller');
-
-var getCheckSession = function (previousSteps) {
-    return proxyquire('../../lib/middleware/check-progress', {
-        '../util/helpers': {
-            getPreviousSteps: sinon.stub().returns(previousSteps)
-        }
-    });
-}
+    Controller = require('../../lib/controller'),
+    helpers = require('../../lib/util/helpers');
 
 describe('middleware/check-session', function () {
 
-    var req, res, next, controller, steps, checkSession;
+    var req, res, next, controller, steps;
 
     beforeEach(function () {
         req = request();
@@ -25,12 +18,21 @@ describe('middleware/check-session', function () {
             '/two': { next: '/three' },
             '/three': { next: '/four' },
             '/four': {}
-        }
+        };
+        sinon.stub(helpers, 'getRouteSteps').returns(['/one', '/two']);
+    });
+
+    afterEach(function () {
+        helpers.getRouteSteps.restore();
+    });
+
+    it('calls getRouteSteps helper with route and steps', function () {
+        checkSession('/two', controller, steps, '/three');
+        helpers.getRouteSteps.should.have.been.calledWithExactly('/two', steps);
     });
 
     it('calls callback with no arguments if prerequisite steps are complete', function (done) {
         req.sessionModel.set('steps', [ '/one', '/two' ]);
-        checkSession = getCheckSession(['/one', '/two']);
         var middleware = checkSession('/three', controller, steps, '/one');
         middleware(req, res, function (err) {
             expect(err).to.be.undefined;
@@ -40,7 +42,7 @@ describe('middleware/check-session', function () {
 
     it('calls callback with MISSING_PREREQ error code if accessing step that has not had prerequisite steps complete', function (done) {
         req.sessionModel.set('steps', []);
-        checkSession = getCheckSession();
+        helpers.getRouteSteps.returns(['/two']);
         var middleware = checkSession('/three', controller, steps, '/one');
         middleware(req, res, function (err) {
             err.code.should.equal('MISSING_PREREQ');

HMPO/hmpo-form-wizard

test/middleware/spec.check-progress.js
@@ -1,18 +1,11 @@
-var proxyquire = require('proxyquire'),
+var checkProgress = require('../../lib/middleware/check-progress'),
     Model = require('../../lib/model'),
-    Controller = require('../../lib/controller');
-
-var getCheckSession = function (previousSteps) {
-    return proxyquire('../../lib/middleware/check-progress', {
-        '../util/helpers': {
-            getPreviousSteps: sinon.stub().returns(previousSteps)
-        }
-    });
-}
+    Controller = require('../../lib/controller'),
+    helpers = require('../../lib/util/helpers');
 
 describe('middleware/check-session', function () {
 
-    var req, res, next, controller, steps, checkSession;
+    var req, res, next, controller, steps;
 
     beforeEach(function () {
         req = request();
@@ -25,13 +18,22 @@ describe('middleware/check-session', function () {
             '/two': { next: '/three' },
             '/three': { next: '/four' },
             '/four': {}
-        }
+        };
+        sinon.stub(helpers, 'getRouteSteps').returns(['/one', '/two']);
+    });
+
+    afterEach(function () {
+        helpers.getRouteSteps.restore();
+    });
+
+    it('calls getRouteSteps helper with route and steps', function () {
+        checkProgress('/two', controller, steps, '/three');
+        helpers.getRouteSteps.should.have.been.calledWithExactly('/two', steps);
     });
 
     it('calls callback with no arguments if prerequisite steps are complete', function (done) {
         req.sessionModel.set('steps', [ '/one', '/two' ]);
-        checkSession = getCheckSession(['/one', '/two']);
-        var middleware = checkSession('/three', controller, steps, '/one');
+        var middleware = checkProgress('/three', controller, steps, '/one');
         middleware(req, res, function (err) {
             expect(err).to.be.undefined;
             done();
@@ -40,8 +42,8 @@ describe('middleware/check-session', function () {
 
     it('calls callback with MISSING_PREREQ error code if accessing step that has not had prerequisite steps complete', function (done) {
         req.sessionModel.set('steps', []);
-        checkSession = getCheckSession();
-        var middleware = checkSession('/three', controller, steps, '/one');
+        helpers.getRouteSteps.returns(['/two']);
+        var middleware = checkProgress('/three', controller, steps, '/one');
         middleware(req, res, function (err) {
             err.code.should.equal('MISSING_PREREQ');
             done();