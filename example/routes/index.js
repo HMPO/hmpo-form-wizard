3147c6aaa6753db7449d4be13e02c96b10baf1c3
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2017-01-12 16:35:57 +0000

    Update example project to run with newest modules

--- example/routes/index.js
+++ example/routes/index.js
@@ -1,10 +1,13 @@
-var steps = require('./steps'),
+var express = require('express'),
+    templateMixins = require('hmpo-template-mixins'),
+    wizard = require('../../'),
+    steps = require('./steps'),
     fields = require('./fields');
 
-var app = require('express').Router();
+var app = express.Router();
 
-app.use(require('hmpo-template-mixins')(fields));
+app.use(templateMixins(fields));
 
-app.use(require('../../')(steps, fields));
+app.use(wizard(steps, fields, { templatePath: 'pages' }));
 
 module.exports = app;
